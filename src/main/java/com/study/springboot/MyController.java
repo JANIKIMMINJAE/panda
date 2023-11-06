a03ccacffa43ea12c56b412654b7a36d9d0b0c75
DESKTOP-EUG5FQP\BigData <tenacitymrtest@gmail.com>
2023-11-06 18:24:22 +0900

    23-11-06 메인페이지 제작중

--- src/main/java/com/study/springboot/MyController.java
+++ src/main/java/com/study/springboot/MyController.java
@@ -2,12 +2,11 @@ package com.study.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
 	@RequestMapping("/")
-	public @ResponseBody String root() throws Exception{
+	public String root() throws Exception{
 		return "mainhome";
 	}
 }
